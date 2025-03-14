592f806cbece6a89862387c67da948074d164f1d
anishau <anishau@uw.edu>
2025-03-14 11:29:58 -0400

    added  auth

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+require('dotenv').config()
+
 module.exports = function(eleventyConfig) {
+    // Pass environment variables to templates
+    eleventyConfig.addGlobalData("env", {
+        supabaseUrl: process.env.SUPABASE_URL,
+        supabaseAnonKey: process.env.SUPABASE_ANON_KEY
+    });
+
     return {
         dir: {
             input: "src",
